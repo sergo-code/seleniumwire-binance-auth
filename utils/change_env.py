c7e7a20424ac4e7ea2108657744392de4388c96f
sergo-code <s.ivanoff-71@yandex.ru>
2022-10-10 17:54:08 +0300

    Updated file .env

--- utils/change_env.py
+++ utils/change_env.py
@@ -6,9 +6,9 @@ def change_env_config(tokens):
     with open(".env", "r") as file:
         for line in file:
             if 'P20T' in line:
-                line = re.sub(r"P20T=web.\d+\.\w+", f'P20T={tokens["p20t_token"]}', line)
+                line = re.sub(r"P20T=", f'P20T={tokens["p20t_token"]}', line)
             elif 'CSRF' in line:
-                line = re.sub(r"CSRF=\w+", f'CSRF={tokens["csrf_token"]}', line)
+                line = re.sub(r"CSRF=", f'CSRF={tokens["csrf_token"]}', line)
             config.append(line)
 
     with open(".env", "w") as file:
